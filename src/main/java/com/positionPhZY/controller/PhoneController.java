10aa10b618661f88dc744e0b3513b5727195060d
handongqingxue <1417140290@qq.com>
2021-12-03 14:10:44 +0800

    完善

--- src/main/java/com/positionPhZY/controller/PhoneController.java
+++ src/main/java/com/positionPhZY/controller/PhoneController.java
@@ -94,12 +94,6 @@ public class PhoneController {
 		else if("syncDBManager".equals(page)){
 			url=MODULE_NAME+"/syncDBManager";
 		}
-		else if("testMap".equals(page)){
-			url=MODULE_NAME+"/testMap";
-		}
-		else if("testModel".equals(page)){
-			url=MODULE_NAME+"/testModel";
-		}
 		else {
 			url="redirect:goPage?page=login";
 		}
